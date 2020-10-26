1a928dd4293af8b0c0bc3bacc1d8995836d8faa7
Irene Chae <jchae2@hotmail.com>
2020-10-26 14:21:25 -0400

    The cancer hotspot has a graph in the detail page. New fields added to the Variant model. Gene report and variant report are now common among each categories.

--- api/models.py
+++ api/models.py
@@ -20,17 +20,47 @@ class Gene(models.Model):
 
 
 class Variant(models.Model):
-	genome_build = models.CharField(max_length=10, null=True)
-	chromosome = models.CharField(max_length=6, null=True)
+	genome_build = models.CharField(verbose_name="genome build", max_length=10, null=True)
+	chr = models.CharField(verbose_name="Chromosome", max_length=6, null=True)
 	start = models.CharField(max_length=10, null=True)
 	end = models.CharField(max_length=10, null=True)
 	ref = models.CharField(max_length=100, null=True)
 	alt = models.CharField(max_length=100, null=True)
 	transcript = models.CharField(max_length=20, null=True)
-	c = models.CharField(max_length=10, null=True)
-	p = models.CharField(max_length=20, null=True)
+	cdna = models.CharField(verbose_name="c.", max_length=10, null=True)
+	protein = models.CharField(verbose_name="p.", max_length=20, null=True)
 	consequence = models.CharField(max_length=10, null=True)
+	exonic_function = models.CharField(max_length=20, null=True)
+	content = models.TextField(blank=True)
+
+	af = models.CharField(verbose_name="AF", max_length=20, null=True)
+	af_popmax = models.CharField(verbose_name="AF_popmax", max_length=20, null=True)
+	cosmic70 = models.CharField(max_length=20, null=True)
+	clinvar = models.CharField(verbose_name="CLINVAR", max_length=20, null=True)
+	insilicodamaging = models.CharField(verbose_name="InSilicoDamaging", max_length=20, null=True)
+	insilicobenign = models.CharField(verbose_name="InSilicoBenign", max_length=100, null=True)
+	polyphen2_hdiv_pred = models.CharField(verbose_name="Polyphen2_HDIV_pred", max_length=4, null=True)
+	polyphen2_hvar_pred = models.CharField(verbose_name="Polyphen2_HVAR_pred", max_length=4, null=True)
+	sift_pred = models.CharField(verbose_name="SIFT_pred", max_length=4, null=True)
+	mutationtaster_pred = models.CharField(verbose_name="MutationTaster_pred", max_length=4, null=True)
+	mutationassessor_pred = models.CharField(verbose_name="MutationAssessor_pred", max_length=4, null=True)
+	provean_pred = models.CharField(verbose_name="PROVEAN_pred", max_length=4, null=True)
+	lrt_pred = models.CharField(verbose_name="LRT_pred", max_length=4, null=True)
+	tcga = models.CharField(verbose_name="TCGA#occurances", max_length=20, null=True)
+	oncokb = models.CharField(verbose_name="oncoKB", max_length=500, null=True)
+	oncokb_pmids = models.CharField(verbose_name="oncoKB_PMIDs", max_length=50, null=True)
+	watson = models.CharField(max_length=20, null=True)
+	watson_pmids = models.CharField(verbose_name="Watson_PMIDs", max_length=50, null=True)
+	qci = models.CharField(verbose_name="QCI", max_length=20, null=True)
+	qci_pmids = models.CharField(verbose_name="QCI_PMIDs", max_length=50, null=True)
+	pmkb = models.CharField(verbose_name="PMKB", max_length=10, null=True)
+	pmkb_citations = models.CharField(verbose_name="PMKB_citations", max_length=100, null=True)
+	civic = models.CharField(verbose_name="CIViC", max_length=50, null=True)
+	google = models.CharField(max_length=100, null=True)
+	alamut = models.CharField(max_length=70, null=True)
+
 	gene = models.ForeignKey(Gene, related_name="variants", on_delete=models.CASCADE, null=True, blank=True)
+	existing = models.ForeignKey('self', related_name="variants", on_delete=models.CASCADE, null=True, blank=True)
 	branch = models.CharField(choices=(
 		('gp', "Genomic Pathogenetic"),
 		('so', "Somatic Oncogenetic"),
@@ -40,6 +70,12 @@ class Variant(models.Model):
 		('c', "Checked but not reported"),
 		('r', "Reported"),
 	), max_length=1, default='n')
+
+
+class CancerHotspot(models.Model):
+	hotspot = models.CharField(max_length=70, null=True)
+	count = models.IntegerField(default=1)
+	variant = models.ForeignKey(Variant, related_name="hotspots", on_delete=models.CASCADE, null=True, blank=True)
 
 
 class PathItem(models.Model):
@@ -128,6 +164,8 @@ class SubEvidence(models.Model):
 class Report(models.Model):
 	name = models.CharField(max_length=40)
 	content = models.TextField(null=True)
+	gene = models.ForeignKey(Gene, related_name="reports", on_delete=models.CASCADE, null=True, blank=True)
+	variant = models.ForeignKey(Variant, related_name="reports", on_delete=models.CASCADE, null=True, blank=True)
 	disease = models.ForeignKey(Disease, related_name="reports", on_delete=models.CASCADE, null=True, blank=True)
 
 	def __str__(self):
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.forms.models import model_to_dict
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404, render
@@ -9,11 +10,10 @@ from django.urls import reverse
 
 import datetime
 import pandas
-import math
 import urllib
 from weasyprint import HTML
 from api.models import *
-from .tables import GeneTable, VariantTable, DiseaseTable, HistoryTable, add_evidence
+from .tables import GeneTable, VariantTable, UploadTable, DiseaseTable, HistoryTable, add_evidence
 
 
 # Create your views here.
@@ -54,9 +54,9 @@ def variants(request):
 
 
 @login_required
-def gene(request, gene_id):
+def gene(request, gene_name):
 	try:
-		item = Gene.objects.get(pk=gene_id)
+		item = Gene.objects.get(name=gene_name)
 	except Gene.DoesNotExist:
 		raise Http404("Gene does not exist")
 	variant_list = VariantTable(item.variants.all())
@@ -64,9 +64,9 @@ def gene(request, gene_id):
 
 
 @login_required
-def variant(request, variant_id):
+def variant(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(gene__name=gene_name, protein=variant_p)
 		score_items = PathItem.objects.all()
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
@@ -78,33 +78,67 @@ def variant(request, variant_id):
 
 @login_required
 def upload(request):
-	exists_dict = {"yes":[], "no":[]}
-	spreadsheet = request.FILES.get("file")
-	if spreadsheet.name.split('.')[-1] == "csv":
-		raw_data = pandas.read_csv(request.FILES.get("file"))
-	else:
-		raw_data = pandas.read_excel(request.FILES.get("file"))
+	if "add_or_update" in request.POST:
+		variant_ids = request.POST.get("variant_ids").split(",")
+		for variant_id in variant_ids:
+			if variant_id in request.POST.getlist("add_or_update"):
+				variant_item = Variant.objects.get(pk=variant_id)
+				if variant_item.existing:
+					existing = variant_item.existing
+					data = model_to_dict(variant_item)
+					data.pop('id', None)
+					data.pop('existing', None)
+					existing.update(**data)
+					variant_item.delete()
+			else:
+				Variant.objects.get(pk=int(variant_id)).delete()
+		return HttpResponseRedirect(reverse('index'))
+	exists_dict = {"yes": [], "no": []}
+	raw_data = pandas.read_excel(request.FILES.get("file"), dtype=str)
+	raw_data.fillna('na', inplace=True)
+
+	variant_ids = []
+	raw_data.rename(columns={'ExonicFunc.UHNCLGGene':'exonic_function'}, inplace=True)
+	raw_data.columns = raw_data.columns.str.lower()
 	for _, row in raw_data.iterrows():
-		exist_variants = Variant.objects.filter(gene__name=row.get("gene")).filter(Q(c=row.get("c")) | Q(p=row.get("p")))
+		if "chr" not in row[0]:
+			continue
+		exist_variants = Variant.objects.filter(gene__name=row.get("gene")).filter(Q(cdna=row.get("cdna")) | Q(protein=row.get("protein")))
 		gene_name = row.pop('gene')
+		raw_hopspots = str(row.pop('cancerhotspots')).split("|")
+
 		try:
 			gene_item = Gene.objects.get(name=gene_name)
 		except Gene.DoesNotExist:
 			gene_item = Gene.objects.create(name=gene_name, pub_date=datetime.datetime.now())
+
+		variant = Variant.objects.create(branch=request.POST.get("data_type"), gene=gene_item, **row)
 		if "consequence" in row or exist_variants.count() == 0:
-			variant = Variant.objects.create(branch=request.POST.get("data_type"), gene=gene_item, **row)
 			History.objects.create(content="Upload", user=request.user, timestamp=datetime.datetime.now(), variant=variant)
 			exists_dict["no"].append(variant)
 		else:
-			exists_dict["yes"].append(exist_variants.first())
-	return render(request, 'general/uploaded.html', {'exist': VariantTable(exists_dict["yes"]), 'new': VariantTable(exists_dict["no"]), 'title': 'Uploads'})
-	#return HttpResponseRedirect(reverse('index'))
+			variant.existing = exist_variants.first()
+			exists_dict["yes"].append(variant)
+		variant_ids.append(str(variant.id))
+		for hotspot in raw_hopspots:
+			values = hotspot.split(":")
+			cancer = CancerHotspot.objects.create(hotspot=values[0], variant=variant)
+			if len(values) > 1:
+				cancer.count = int(values[1])
+				cancer.save()
+
+	return render(request, 'general/uploaded.html', {'exist': UploadTable(exists_dict["yes"]), 'new': UploadTable(exists_dict["no"]), "variant_ids": ",".join(variant_ids), 'title': 'Uploads'})
 
 
 @login_required
-def save(request, variant_id):
+def save(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(gene__name=gene_name, protein=variant_p)
+		gene1 = Gene.objects.get(name=gene_name)
+		gene1.content = request.POST.get("gene_report")
+		gene1.save()
+		item.content = request.POST.get("variant_report")
+		item.save()
 
 		i = 1
 		while request.POST.get("d" + str(i) + "_disease"):
@@ -143,34 +177,33 @@ def save(request, variant_id):
 			i += 1
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
-	return HttpResponseRedirect(reverse('variant_text', args=(variant_id,)))
+	return HttpResponseRedirect(reverse('variant_text', args=(gene_name,variant_p)))
 
 
 @login_required
-def variant_text(request, variant_id):
+def variant_text(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(protein=variant_p, gene__name=gene_name)
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
 	return render(request, 'variants/' + item.branch + '_detail.html', {'item': item})
 
 
 @login_required
-def export(request, variant_id):
+def export(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(gene__name=gene_name, protein=variant_p)
 		disease_list = DiseaseTable(item.diseases.all())
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
 	return render(request, 'variants/index.html', {'title': 'Export for Variant', 'item': item, 'table': disease_list})
 
 
-def exported(request, variant_id):
+def exported(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(gene__name=gene_name, protein=variant_p)
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
-	#print(request.POST.getlist("disease"))
 	diseases = Disease.objects.filter(name__in=request.POST.getlist("disease"))
 	if item.branch == 'gp':
 		html_string = render_to_string('general/gp_export.html', {'item': item, 'diseases': diseases})
@@ -187,9 +220,9 @@ def exported(request, variant_id):
 		return response
 
 
-def history(request, variant_id):
+def history(request, gene_name, variant_p):
 	try:
-		item = Variant.objects.get(pk=variant_id)
+		item = Variant.objects.get(gene__name=gene_name, protein=variant_p)
 		histories = HistoryTable([h for h in item.history.all()])
 	except Variant.DoesNotExist:
 		raise Http404("Variant does not exist")
