7582cf361e4d903dfa9f5df1cc7955e21fc90df8
Irene Chae <jchae2@hotmail.com>
2020-10-29 17:24:00 -0400

    Undone the custom_tag commit.

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -7,8 +7,3 @@ register = template.Library()
 @register.filter(name='get_class')
 def get_class(value):
 	return value.__class__.__name__
-
-
-@register.filter(name='get_disease')
-def get_disease(name):
-	return [disease.name for disease in Disease.objects.filter(name=name)]
