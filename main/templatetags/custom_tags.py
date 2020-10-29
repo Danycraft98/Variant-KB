6e51d448a05afc7c9a9fd84f297e5f1b2608fe57
Irene Chae <jchae2@hotmail.com>
2020-10-29 17:23:29 -0400

    Added reviewed and approved checkboxes.

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from api.models import Disease
 
 register = template.Library()
 
@@ -6,3 +7,8 @@ register = template.Library()
 @register.filter(name='get_class')
 def get_class(value):
 	return value.__class__.__name__
+
+
+@register.filter(name='get_disease')
+def get_disease(name):
+	return [disease.name for disease in Disease.objects.filter(name=name)]
