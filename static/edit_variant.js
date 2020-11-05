30c775045fbc3fd9c60f52c53a937a4862457117
Irene Chae <jchae2@hotmail.com>
2020-11-05 09:31:20 -0500

    Added confirm message to update.

---
+++ static/edit_variant.js
@@ -0,0 +1,8 @@
+function updateMsg() {
+    var checkboxes = document.querySelectorAll('input[name="review"]:checked');
+    console.log(checkboxes)
+    if (checkboxes.length > 0) {
+        return confirm('Do you want to update?');
+    }
+    return true;
+}
